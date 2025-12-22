7bfba55d9ffdf1da6ec13912ff3e7abf64f7e3a2
romainaugier <augier.romain@sfr.fr>
2025-12-22 19:03:36 +0100

    Added arm support in custom cmake set_target_options

--- cmake/target_options.cmake
+++ cmake/target_options.cmake
@@ -1,39 +1,80 @@
 function(set_target_options target_name)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    if(CMAKE_SYSTEM_PROCESSOR MATCHES "^(aarch64|arm64|ARM64)")
+        set(IS_ARM TRUE)
+    elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "^(x86_64|AMD64|amd64)")
+        set(IS_ARM FALSE)
+    else()
+        set(IS_ARM FALSE)
+        message(WARNING "Unknown processor architecture: ${CMAKE_SYSTEM_PROCESSOR}, assuming x86_64")
+    endif()
+
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang" OR CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
         if(${SANITIZE})
             target_compile_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=address>)
             target_link_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=address>)
-
             target_compile_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=undefined>)
             target_link_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=undefined>)
         endif()
 
-        set(COMPILE_OPTIONS -D_FORTIFY_SOURCES=2 -pipe -Wall -pedantic-errors $<$<CONFIG:Release,RelWithDebInfo>:-O3 -ftree-vectorizer-verbose=2> -mveclibabi=svml -mavx2 -mfma)
+        set(COMPILE_OPTIONS -D_FORTIFY_SOURCE=2 -pipe -Wall -pedantic-errors $<$<CONFIG:Release,RelWithDebInfo>:-O3>)
+
+        if(IS_ARM)
+            list(APPEND COMPILE_OPTIONS -mcpu=apple-m1)
+            if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+                # Apple Silicon NEON optimizations are enabled by default with -mcpu=apple-m1
+            else()
+                # For non-Apple ARM (Linux ARM64), enable NEON
+                list(APPEND COMPILE_OPTIONS -march=armv8-a+fp+simd)
+            endif()
+        else()
+            if(CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+                list(APPEND COMPILE_OPTIONS -mavx2 -mfma)
+            else()
+                list(APPEND COMPILE_OPTIONS $<$<CONFIG:Release,RelWithDebInfo>:-ftree-vectorizer-verbose=2> -mveclibabi=svml -mavx2 -mfma)
+            endif()
+        endif()
 
         target_compile_options(${target_name} PRIVATE ${COMPILE_OPTIONS})
-    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
         if(${SANITIZE})
             target_compile_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=address>)
             target_link_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=address>)
-
             target_compile_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=undefined>)
             target_link_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:-fsanitize=undefined>)
         endif()
 
-        set(COMPILE_OPTIONS -D_FORTIFY_SOURCES=2 -pipe -Wall -pedantic-errors $<$<CONFIG:Release,RelWithDebInfo>:-O3 -ftree-vectorizer-verbose=2> -mveclibabi=svml -mavx2 -mfma)
+        set(COMPILE_OPTIONS -D_FORTIFY_SOURCE=2 -pipe -Wall -pedantic-errors $<$<CONFIG:Release,RelWithDebInfo>:-O3 -ftree-vectorizer-verbose=2>)
+
+        if(IS_ARM)
+            list(APPEND COMPILE_OPTIONS -march=armv8-a+fp+simd -mtune=generic)
+        else()
+            list(APPEND COMPILE_OPTIONS -mveclibabi=svml -mavx2 -mfma)
+        endif()
 
         target_compile_options(${target_name} PRIVATE ${COMPILE_OPTIONS})
-    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
-        message(FATAL "Intel compiler is not supported")
-    elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-        include(find_avx)
 
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+        message(FATAL_ERROR "Intel compiler is not supported")
+
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         if(${SANITIZE})
             target_compile_options(${target_name} PRIVATE $<$<CONFIG:Debug,RelWithDebInfo>:/fsanitize=address>)
         endif()
 
-        set(COMPILE_OPTIONS /W4 /utf-8 ${AVX_FLAGS} $<$<CONFIG:Release,RelWithDebInfo>:/O2 /GF /Ot /Oy /GT /GL /Oi /Zi /Gm- /Zc:inline>)
+        set(COMPILE_OPTIONS /W4 /utf-8 $<$<CONFIG:Release,RelWithDebInfo>:/O2 /GF /Ot /Oy /GT /GL /Oi /Zi /Gm- /Zc:inline>)
 
+        if(IS_ARM)
+            # MSVC ARM64 optimizations are mostly automatic
+        else()
+            include(find_avx)
+            list(APPEND COMPILE_OPTIONS ${AVX_FLAGS})
+        endif()
+
+        target_compile_options(${target_name} PRIVATE ${COMPILE_OPTIONS})
+    else()
+        message(WARNING "Unknown compiler: ${CMAKE_CXX_COMPILER_ID}, using minimal options")
+        set(COMPILE_OPTIONS -O3 -Wall)
         target_compile_options(${target_name} PRIVATE ${COMPILE_OPTIONS})
     endif()
 
