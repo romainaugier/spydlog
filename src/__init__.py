d1add145cd709c19f28a3f71fabcef504ed17d87
romainaugier <augier.romain@sfr.fr>
2025-12-22 19:47:46 +0100

    Added version handling in release action

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,10 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    try:
+        from importlib.metadata import version as _get_version
+        __version__ = _get_version("spydlog")
+    except Exception:
+        __version__ = "0.0.0+unknown"
+
 from .spydlog import *
