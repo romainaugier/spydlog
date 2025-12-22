762c5d23c2259f6c639908bc6d1f51ddc44a5f3e
romainaugier <augier.romain@sfr.fr>
2025-12-22 19:52:31 +0100

    Added __init__.py in tests directory

--- src/_version.py
+++ src/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.0.dev18'
-__version_tuple__ = version_tuple = (0, 1, 0, 'dev18')
+__version__ = version = '1.0.1.dev2'
+__version_tuple__ = version_tuple = (1, 0, 1, 'dev2')
 
-__commit_id__ = commit_id = 'gf826380a9'
+__commit_id__ = commit_id = 'gc2ee6f8e7'
